eeedf5f1a601a0cc8c62a9c50154e3cb7da1799a
Maria Victoria <marivibilbaog@gmail.com>
2021-10-05 22:21:22 +0200

    Agrego algunos cambios

--- myapp/server.js
+++ myapp/server.js
@@ -15,6 +15,7 @@ app.use(myLogger);
 app.get("/add", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
+  const result = value1 + value2;
   res.send(`Result:` + result);
 });
 
@@ -28,19 +29,22 @@ app.get("/addparams/:value1/:value2", (req, res) => {
 app.get("/substract", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
-  res.send(`Result:` - result);
+  const result = value1 - value2;
+  res.send(`Result:`+ result);
 });
 
 app.get("/multiply", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
-  res.send(`Result:` * result);
+  const result = value1 * value2;
+  res.send(`Result:` + result);
 });
 
 app.get("/divide", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
-  res.send(`Result:` / result);
+  const result = value1 / value2;
+  res.send(`Result:` + result);
 });
 
 app.listen(3000, () => console.log("Server is up and running"));
