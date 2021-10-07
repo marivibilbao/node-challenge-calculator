dd0fbb90595dd40cab590e3f526e782bebc05dc9
Maria Victoria <marivibilbaog@gmail.com>
2021-10-07 20:22:48 +0200

    Agrego modificaciones

--- myapp/server.js
+++ myapp/server.js
@@ -12,13 +12,36 @@ const myLogger = (req, res, next) => {
 };
 app.use(myLogger);
 
+//Forma vieja
 app.get("/add", (req, res) => {
-  const value1 = parseInt(req.params.value1);
-  const value2 = parseInt(req.params.value2);
+  const value1 = parseInt(req.query.value1);
+  const value2 = parseInt(req.query.value2);
   const result = value1 + value2;
   res.send(`Result:` + result);
 });
 
+app.get("/substract", (req, res) => {
+  const value1 = parseInt(req.query.value1);
+  const value2 = parseInt(req.query.value2);
+  const result = value1 - value2;
+  res.send(`Result:`+ result);
+});
+
+app.get("/multiply", (req, res) => {
+  const value1 = parseInt(req.query.value1);
+  const value2 = parseInt(req.query.value2);
+  const result = value1 * value2;
+  res.send(`Result:` + result);
+});
+
+app.get("/divide", (req, res) => {
+  const value1 = parseInt(req.query.value1);
+  const value2 = parseInt(req.query.value2);
+  const result = value1 / value2;
+  res.send(`Result:` + result);
+});
+
+//Forma nueva
 app.get("/addparams/:value1/:value2", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
@@ -26,25 +49,26 @@ app.get("/addparams/:value1/:value2", (req, res) => {
   res.send(`Result:` + result);
 });
 
-app.get("/substract", (req, res) => {
+app.get("/substractparams/:value1/:value2", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
   const result = value1 - value2;
   res.send(`Result:`+ result);
 });
 
-app.get("/multiply", (req, res) => {
+app.get("/multiplyparams/:value1/:value2", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
   const result = value1 * value2;
   res.send(`Result:` + result);
 });
 
-app.get("/divide", (req, res) => {
+app.get("/divideparams/:value1/:value2", (req, res) => {
   const value1 = parseInt(req.params.value1);
   const value2 = parseInt(req.params.value2);
   const result = value1 / value2;
   res.send(`Result:` + result);
 });
+
 
 app.listen(3000, () => console.log("Server is up and running"));
